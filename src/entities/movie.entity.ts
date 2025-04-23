1d2f67d487da10511cb6b5b5e84c723d7abbc414
Islam Mušić <islam.music@scv.si>
2025-04-23 09:14:09 +0200

    update, delete genre urejen

---
+++ src/entities/movie.entity.ts
@@ -0,0 +1,17 @@
+import {Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
+
+@Entity('movies')
+export class Movie {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column()
+    name: string;
+    @Column({nullable: true})
+    description: string;
+    @Column({nullable: true})
+    releaseDate: Date;
+    @CreateDateColumn()
+    createdAt: Date;
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
--- src/genre/genre.service.ts
+++ src/genre/genre.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import {InjectRepository} from "@nestjs/typeorm";
-import {Repository} from "typeorm";
+import {DeleteResult, Repository} from "typeorm";
 import {Genre} from "../entities/genre.entity";
 import {CreateGenreDto} from "./create-genre.dto";
+import {UpdateGenreDto} from "./update-genre.dto";
 
 @Injectable()
 export class GenreService {
@@ -15,4 +16,23 @@ export class GenreService {
         const genre = this.genreRepository.create(createGenreDto);
         return this.genreRepository.save(genre);
     }
+
+    async update(id:number,updateGenreDto:UpdateGenreDto):Promise<Genre | null> {
+        await this.genreRepository.update(id,updateGenreDto);
+        return this.genreRepository.findOneBy({id});
+    }
+
+    findAll():Promise<Genre[]> {
+        return this.genreRepository.find();
+    }
+
+    findeOne(id: number):Promise<Genre | null> {
+        return this.genreRepository.findOneBy({id})
+    }
+
+    delete(id:number):Promise<DeleteResult> {
+        return this.genreRepository.delete({id});
+    }
+
+
 }
